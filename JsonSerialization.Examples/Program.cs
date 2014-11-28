c6b9f6a0a694cb04de316b5e8f6a7e58ed832d1d
Raphael Haddad <raphael@haddad.net.au>
2014-11-28 13:58:25 +1100

    removing console.writeline

--- JsonSerialization.Examples/Program.cs
+++ JsonSerialization.Examples/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json.Serialization.ContractResolverExtentions;
 
 namespace JsonSerialization.Examples
@@ -27,28 +28,19 @@ namespace JsonSerialization.Examples
 
             var json = JsonConvert.SerializeObject(data, jsonSettings);//serialize object to snake_case1
 
-            Console.WriteLine(json);//without seperated integers
-
             var obj = JsonConvert.DeserializeObject<MyClass>(json, jsonSettings);//desserialize object from snake_case1
-
-            Console.WriteLine(obj.ToString());
 
             jsonSettings.ContractResolver = new SnakeCaseIntegerSeperatedPropertyNamesContractResolver();
 
             json = JsonConvert.SerializeObject(data, jsonSettings);//serialize object to snake_case_2
 
-            Console.WriteLine(json);//with seperated integers
-
             obj = JsonConvert.DeserializeObject<MyClass>(json,jsonSettings); //deserialize object from snake_case_2
-
-            Console.WriteLine(obj.ToString());
 
             //change settings to snake_case
             jsonSettings.ContractResolver = new SnakeCasePropertyNamesContractResolver();
 
             obj = JsonConvert.DeserializeObject<MyClass>(json, jsonSettings);//deserialization does not work for all fields
 
-            Console.WriteLine(obj);
         }
     }
 }
