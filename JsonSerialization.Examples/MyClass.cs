65b3a800dd235883f4e45672c9f171871b8bfa3d
Raphael Haddad <raphael@haddad.net.au>
2014-11-28 11:44:39 +1100

    init

---
+++ JsonSerialization.Examples/MyClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSerialization.Examples
+{
+    public class MyClass
+    {
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string FirstName1
+        {
+            get;
+            set;
+        }
+
+        public string First1Name
+        {
+            get;
+            set;
+        }
+
+        public string FName
+        {
+            get;
+            set;
+        }
+
+        public string _FirstName
+        {
+            get;
+            set;
+        }
+
+        public string FirstName_
+        {
+            get;
+            set;
+        }
+
+        public string _FName
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ JsonSerialization.Examples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization.ContractResolverExtentions;
+
+namespace JsonSerialization.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new MyClass
+            {
+                FName = "Raphael",
+                First1Name = "Raph",
+                FirstName = "Raph",
+                FirstName1 = "Raph",
+                FirstName_ = "Raph",
+                _FName = "Raph"
+            };
+
+            var jsonSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new SnakeCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            var json = JsonConvert.SerializeObject(data, jsonSettings);
+
+            Console.WriteLine(json);//without seperated integers
+        }
+    }
+}
